be269622e4f6c6df3f106408dac022adf671d2f5
tomasborek <tomasborek3@gmail.com>
2022-05-16 10:38:20 +0200

    Ignoring padding fix

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = ({ width, opacity, absolute, center, z }) => {
       src="/img/logo/osmicka.svg"
       alt="eight"
       style={{ maxWidth: width }}
-      className={`max-w-full max-h-full select-none opacity-${opacity} z-${z} ${
+      className={`w-full max-h-full select-none opacity-${opacity} z-${z} ${
         absolute ? "absolute top-0 bottom-0 w-full" : ""
       } ${center ? "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" : ""}`}
     />
