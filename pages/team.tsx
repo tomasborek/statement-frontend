05f89bb982b94b2ad9c4baf01e80535644c1e2fb
tomasborek <tomasborek3@gmail.com>
2022-05-17 09:45:32 +0200

    Team swipe

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import WhyUs from "../components/WhyUs";
 import Footer from "../components/Footer";
@@ -7,6 +7,19 @@ import { motion } from "framer-motion";
 import TeamMember from "../components/TeamMember";
 import Head from "next/head";
 const Team = () => {
+  const [vw, setVw] = useState(0);
+  useEffect(() => {
+    setVw(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    setVw(window.innerWidth);
+  };
+
   return (
     <>
       <Head>
@@ -22,19 +35,31 @@ const Team = () => {
             </h1>
           </header>
           {/* Team people */}
-          <div className="flex relative z-20 justify-center">
-            <TeamMember
-              name={"Pavel Hašek"}
-              img={"pavel_team_holo.png"}
-              description={"Marketing manager"}
-              resize
-            />
-            <TeamMember
-              name={"Tomáš Bořek"}
-              img={"tomas_team_holo.png"}
-              description={"Developer"}
-              resize
-            />
+          <div className="overflow-hidden main-container">
+            <motion.div
+              {...(vw < 700 ? { drag: "x" } : {})}
+              dragConstraints={{ right: -250, left: 250 }}
+              className="flex relative z-20 justify-center"
+            >
+              <TeamMember
+                name={"Pavel Hašek"}
+                img={"pavel_team_holo.png"}
+                description={"Marketing manager"}
+                resize
+              />
+              <TeamMember
+                name={"Tomáš Bořek"}
+                img={"tomas_team_holo.png"}
+                description={"Developer"}
+                resize
+              />
+              <TeamMember
+                name={"Josef Hemroch"}
+                img={"joe_team_holo.png"}
+                description={"IT specialista"}
+                resize
+              />
+            </motion.div>
           </div>
 
           <div className="green-gradient absolute bottom-0 top-0 w-full z-10"></div>
