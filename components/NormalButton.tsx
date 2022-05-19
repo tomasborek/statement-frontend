9d5a3ffa78ac2bae391e0794ec2875181d7b789a
tomasborek <tomasborek3@gmail.com>
2022-05-19 15:49:32 +0200

    Tlačítko chci se sejít je nyní funkční

--- components/NormalButton.tsx
+++ components/NormalButton.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 const NormalButton = ({ children, handleClick, size }) => {
-  return (
-    <button
-      onClick={() => (handleClick ? handleClick : "")}
-      className={`border-2 text-${size} border-lightGreen rounded-md py-2 px-4 text-lightGreen hover:bg-lightGreen hover:text-bg transition-all font-bold`}
-    >
-      {children}
-    </button>
-  );
+  if (handleClick) {
+    return (
+      <button
+        onClick={handleClick}
+        className={`border-2 text-${size} border-lightGreen rounded-md py-2 px-4 text-lightGreen hover:bg-lightGreen hover:text-bg transition-all font-bold`}
+      >
+        {children}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className={`border-2 text-${size} border-lightGreen rounded-md py-2 px-4 text-lightGreen hover:bg-lightGreen hover:text-bg transition-all font-bold`}
+      >
+        {children}
+      </button>
+    );
+  }
 };
 
 NormalButton.defaultProps = {
