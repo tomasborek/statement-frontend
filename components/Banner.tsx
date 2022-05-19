7669c52eadf0bf53d176106e379edb711effc92e
tomasborek <tomasborek3@gmail.com>
2022-05-19 15:41:32 +0200

    Byl odebrán nepotřebný black gradient banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -43,7 +43,6 @@ const Banner = ({ scrollToRef }) => {
         </GlowingButton>
       </div>
       <div className="w-full absolute top-0 left-0 bottom-0 z-20 md:z-10 transparent-responsive-green-gradient"></div>
-      <div className="black-gradient absolute top-0 bottom-0 left-0 right-0"></div>
       <video
         playsInline
         ref={bannerContentRef}
