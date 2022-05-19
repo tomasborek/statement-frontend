99158b10d6e75507c9efa0267fb113b3bdb1c836
tomasborek <tomasborek3@gmail.com>
2022-05-19 15:38:33 +0200

    Respo banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -23,14 +23,9 @@ const Banner = ({ scrollToRef }) => {
   }, []);
 
   return (
-    <div className="relative w-full h-screen flex flex-col justify-center items-center overflow-hidden">
+    <div className="relative w-full h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
       <div className="relative z-20 w-full flex flex-col items-center justify-center flex-1">
         <div className="relative w-full px-4 mb-8 flex justify-center">
-          {/* <img
-            src="/img/logo/osmicka.svg"
-            alt="eight"
-            className="max-w-[400px] w-full px-4 opacity-50 mb-8"
-          /> */}
           <Logo width={400} />
         </div>
         <h1 className="text-5xl text-center mb-4">Vize jsou nekonečné</h1>
@@ -47,7 +42,7 @@ const Banner = ({ scrollToRef }) => {
           Služby
         </GlowingButton>
       </div>
-      <div className="w-full absolute top-0 left-0 bottom-0 z-10 green-gradient"></div>
+      <div className="w-full absolute top-0 left-0 bottom-0 z-20 md:z-10 transparent-responsive-green-gradient"></div>
       <div className="black-gradient absolute top-0 bottom-0 left-0 right-0"></div>
       <video
         playsInline
@@ -56,10 +51,8 @@ const Banner = ({ scrollToRef }) => {
         autoPlay
         loop
         muted
-        className="w-full select-none object-cover h-full absolute  opacity-10 z-10"
+        className="w-full h-3/4 md:h-full select-none object-cover absolute opacity-30 md:opacity-10 z-10"
       ></video>
-      {/*Triangle*/}
-      {/* <div className=" w-0 h-0 border-b-[100px] border-r-[50vw] border-l-[50vw] border-t-0 border-r-transparent border-l-transparent border-bg z-20"></div> */}
     </div>
   );
 };
