17cd7b4e05ba3e720d7cb0d9e87ea570ed11e196
tomasborek <tomasborek3@gmail.com>
2022-05-21 12:16:41 +0200

    Banner finale

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,14 +6,11 @@ import Logo from "./Logo";
 
 const Banner = ({ scrollToRef }) => {
   const bannerContentRef = useRef(null);
-  const [suspended, setSuspended] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    bannerContentRef.current.addEventListener("suspend", handleSuspend);
     return () => {
       console.log("Removing event listener");
       window.removeEventListener("scroll", handleScroll);
-      bannerContentRef.current.removeEventListener("suspend", handleSuspend);
     };
   }, []);
 
@@ -26,15 +23,11 @@ const Banner = ({ scrollToRef }) => {
       : "";
   };
 
-  const handleSuspend = () => {
-    setSuspended(true);
-  };
-
   return (
-    <div className="relative w-full h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
+    <div className="green-gradient relative w-full h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
       <div className="relative z-20 w-full flex flex-col items-center justify-center flex-1">
-        <div className="relative w-full px-4 my-8 flex justify-center">
-          <Logo width={400} />
+        <div className="relative w-5/6 max-w-[400px] px-4 my-8 flex justify-center">
+          <Logo width={"100%"} />
         </div>
         <h1 className="text-5xl text-center mb-4">Vize jsou nekonečné</h1>
         <h3 className="text-2xl text-center mb-8 font-extralight">
@@ -50,10 +43,10 @@ const Banner = ({ scrollToRef }) => {
           Služby
         </GlowingButton>
       </div>
-      <div className="w-full absolute top-0 left-0 bottom-0 z-20 md:z-10 transparent-responsive-green-gradient"></div>
+      {/* <div className="w-full absolute top-0 left-0 bottom-0 z-20 md:z-10 transparent-responsive-green-gradient"></div> */}
       <img
         ref={bannerContentRef}
-        className="w-full h-full md:h-full select-none object-cover absolute opacity-30 md:opacity-10 z-10"
+        className="w-full h-full md:h-full select-none object-cover absolute opacity-10 z-10"
         src="/video/planeta.webp"
         alt=""
       />
