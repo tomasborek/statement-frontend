0a1c1c89b869d035e049b4e47ee1832ecd5269ec
tomasborek <tomasborek3@gmail.com>
2022-05-22 11:16:42 +0200

    Test verze bug fixu: Video se pauzne pokud nefunguje play

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,8 +4,16 @@ import GlowingButton from "./GlowingButton";
 import Logo from "./Logo";
 
 const Banner = ({ scrollToRef }) => {
-  const bannerContentRef = useRef(null);
+  const video = useRef(null);
   useEffect(() => {
+    video.current
+      .play()
+      .then((response) => {
+        //Success
+      })
+      .catch((error) => {
+        video.current.pause();
+      });
     window.addEventListener("scroll", handleScroll);
     return () => {
       console.log("Removing event listener");
@@ -15,16 +23,16 @@ const Banner = ({ scrollToRef }) => {
 
   const handleScroll = () => {
     const offset = window.pageYOffset;
-    const bannerContent = bannerContentRef.current;
+    const videoRef = video.current;
 
-    bannerContent
-      ? (bannerContent.style.transform = `translateY(${offset * 0.5}px)`)
+    videoRef
+      ? (videoRef.style.transform = `translateY(${offset * 0.5}px)`)
       : "";
   };
 
   return (
-    <div className="green-gradient relative w-full h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
-      <div className="relative z-20 w-full flex flex-col items-center justify-center flex-1">
+    <div className="relative w-full h-screen flex flex-col justify-center items-center overflow-hidden bg-black">
+      <div className="relative z-30 w-full flex flex-col items-center justify-center flex-1">
         <div className="relative w-5/6 max-w-[400px] px-4 my-8 flex justify-center">
           <Logo width={"100%"} />
         </div>
@@ -42,11 +50,14 @@ const Banner = ({ scrollToRef }) => {
           Služby
         </GlowingButton>
       </div>
-      <img
-        ref={bannerContentRef}
-        className="w-full h-full  select-none object-cover opacity-10 z-10 absolute"
-        src="/video/planeta.webp"
-        alt="Banner"
+      <div className="absolute w-full top-0 bottom-0 transparent-responsive-green-gradient z-20"></div>
+      <video
+        autoPlay
+        muted
+        playsInline
+        ref={video}
+        className="w-full h-2/3 md:h-full select-none object-cover opacity-30 md:opacity-10 z-10 absolute"
+        src="/video/planeta.webm"
       />
     </div>
   );
